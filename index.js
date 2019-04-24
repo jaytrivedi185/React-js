9606949b19b314ee22770be83db230badca6632f
Jay Trivedi <32363243+jaytrivedi185@users.noreply.github.com>
2019-04-24 22:29:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Greet from "./Component/Greet";
+
+class index extends Component {
+  render() {
+    return (
+      <div className="Index">
+        <Greet />
+      </div>
+    );
+  }
+}
+
+export default index;
